d009f829e7d611a21230835d4359a989ab002c53
PiriyaphongW <piriyaphong1640@gmail.com>
2021-03-19 22:55:55 +0700

    cloud

--- src/main.js
+++ src/main.js
@@ -2,11 +2,30 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { BootstrapVue } from 'bootstrap-vue'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false;
+
+var firebaseConfig = {
+  apiKey: "AIzaSyArCbII5UhBhempnNR1gBmAkDD7c103AOM",
+  authDomain: "cloud-fire-9ead7.firebaseapp.com",
+  projectId: "cloud-fire-9ead7",
+  storageBucket: "cloud-fire-9ead7.appspot.com",
+  messagingSenderId: "793438778482",
+  appId: "1:793438778482:web:fd34b9d528d02b75c17eeb"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export const db = firebase.firestore()
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: h => h(App)
 }).$mount("#app");
---
+++ src/views/firestore.vue
@@ -0,0 +1,139 @@
+<template >
+  <div class="bgi" >
+    <h1 class="t">SIGN UP</h1>
+    <br>
+    <div class="container">
+     
+      <b-form> <br>
+        <b-form-group
+          id="input-group-2"
+          class="tt"
+          label="First Name"
+          label-for="input-2"
+        >
+          <b-form-input
+            id="input-2"
+            v-model="name"
+            type="text"
+            placeholder="ชื่อ "
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-2"
+          class="tt"
+          label="Last Name"
+          label-for="input-2"
+        >
+          <b-form-input
+            id="input-2"
+            v-model="lastname"
+            type="text"
+            placeholder="นามสกุล "
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          class="tt"
+          id="input-group-2"
+          label="Phone Number"
+          label-for="input-2"
+        >
+          <b-form-input
+            id="input-2"
+            v-model="phonenumber"
+            type="text"
+            placeholder="เบอร์โทรศัพท์ "
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          class="tt"
+          id="input-group-1"
+          label="Email Address"
+          label-for="input-1"
+        >
+          <b-form-input
+            id="input-1"
+            v-model="email"
+            type="email"
+            placeholder="อีเมล "
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-2"
+          class="tt"
+          label="Password"
+          label-for="input-2"
+        >
+          <b-form-input
+            id="input-2"
+            v-model="password"
+            placeholder="รหัสผ่าน "
+            type="password"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-button class="btn"  @click="onSubmit"
+          >Sign Up</b-button 
+        >
+      </b-form><br>
+      
+    </div>
+  </div>
+ 
+</template>
+
+<script>
+import { db } from "@/main.js";
+export default {
+  data() {
+    return {
+      Firstname: "",
+      Lastname: "",
+      phonenumber: "",
+      Email: "",
+      Password: "",
+    };
+  },
+  methods: {
+    onSubmit() {
+      var form = {
+        Firstname: this.name,
+        Lastname: this.lastname,
+        Phonenumber: this.phonenumber,
+        Email: this.email,
+        Password: this.password,
+      };
+      db.collection("firestore")
+        .doc()
+        .set(form);
+    },
+  },
+};
+</script>
+
+<style>
+.container {
+  background: rgba(170, 127, 163, 0.89);
+}
+
+
+.tt {
+  margin-left: 17px;
+  text-align: left;
+}
+.btn{
+    color: rgb(90, 147, 212);
+}
+.bgi{
+     background-image: url("https://images.pexels.com/photos/2559941/pexels-photo-2559941.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
+}
+
+</style>
